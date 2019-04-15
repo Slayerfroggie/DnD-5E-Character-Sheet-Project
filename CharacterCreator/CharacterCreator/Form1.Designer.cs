85488aa0b73235a17bb5efb64d220e98cc8f3546
Frank Sluyter <47513385+Slayerfroggie@users.noreply.github.com>
2019-04-15 16:53:16 +1000

    Inital GUI back design no real substance yet

---
+++ CharacterCreator/CharacterCreator/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace CharacterCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cboCharacterName = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cboCharacterName
+            // 
+            this.cboCharacterName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboCharacterName.FormattingEnabled = true;
+            this.cboCharacterName.Location = new System.Drawing.Point(55, 68);
+            this.cboCharacterName.Name = "cboCharacterName";
+            this.cboCharacterName.Size = new System.Drawing.Size(195, 24);
+            this.cboCharacterName.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(701, 908);
+            this.Controls.Add(this.cboCharacterName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboCharacterName;
+    }
+}
+
